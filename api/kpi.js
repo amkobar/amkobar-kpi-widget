8295499acf9ccbcd3fc802ba9463ff9d97cbba41
amkobar <me.amkobar@gmail.com>
2026-03-01 07:47:14 +0700

    Update kpi.js

--- api/kpi.js
+++ api/kpi.js
@@ -54,7 +54,7 @@ data.results.forEach((page) => {
   const status = props["Status Project"]?.select?.name || "";
 
   const riskLevel = props["Risk Level"]?.formula?.string || "";
-  if (riskLevel === "🔴 Overdue") {
+  if (riskLevel === "Overdue") {
     terlambat += 1;
   }
 
