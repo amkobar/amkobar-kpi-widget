75826e9345d73d0b207d9c5de1976481514197c7
amkobar <me.amkobar@gmail.com>
2026-03-14 23:54:10 +0700

    Update kpi.js

--- api/kpi.js
+++ api/kpi.js
@@ -37,7 +37,7 @@ try {
   const data = await response.json()
   if (data.results && data.results.length > 0) {
     const props = data.results[0].properties
-
+console.log("Full props:", JSON.stringify(props))
 
     
     totalRevenue = getNumber(props["Total Revenue (All Time)"])
